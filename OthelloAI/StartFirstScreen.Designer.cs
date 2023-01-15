3df6a06107c3b4794094d9dcfe1409423b0944b0
SirGreen <khoa.huynhk@hcmut.edu.vn>
2023-01-15 10:16:10 +0700

    #1 BP,BR,WP,WR

--- OthelloAI/StartFirstScreen.Designer.cs
+++ OthelloAI/StartFirstScreen.Designer.cs
@@ -34,20 +34,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lbB = new System.Windows.Forms.Label();
             this.lbW = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.timeDown = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.Exitbtn = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label3 = new System.Windows.Forms.Label();
             this.flScoreBoard.SuspendLayout();
             this.SuspendLayout();
             // 
             // PlayBoard
             // 
-            this.PlayBoard.Location = new System.Drawing.Point(18, 18);
-            this.PlayBoard.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PlayBoard.Location = new System.Drawing.Point(12, 12);
             this.PlayBoard.Name = "PlayBoard";
-            this.PlayBoard.Size = new System.Drawing.Size(686, 722);
+            this.PlayBoard.Size = new System.Drawing.Size(457, 469);
             this.PlayBoard.TabIndex = 0;
             // 
             // flScoreBoard
@@ -59,18 +58,20 @@
             this.flScoreBoard.Controls.Add(this.timeDown);
             this.flScoreBoard.Controls.Add(this.label2);
             this.flScoreBoard.Controls.Add(this.Exitbtn);
-            this.flScoreBoard.Location = new System.Drawing.Point(711, 18);
+            this.flScoreBoard.Location = new System.Drawing.Point(474, 12);
+            this.flScoreBoard.Margin = new System.Windows.Forms.Padding(2);
             this.flScoreBoard.Name = "flScoreBoard";
-            this.flScoreBoard.Size = new System.Drawing.Size(267, 375);
+            this.flScoreBoard.Size = new System.Drawing.Size(178, 244);
             this.flScoreBoard.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 46);
+            this.label1.Size = new System.Drawing.Size(90, 31);
             this.label1.TabIndex = 2;
             this.label1.Text = "Score";
             // 
@@ -78,9 +79,10 @@
             // 
             this.lbB.AutoSize = true;
             this.lbB.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbB.Location = new System.Drawing.Point(3, 46);
+            this.lbB.Location = new System.Drawing.Point(2, 31);
+            this.lbB.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbB.Name = "lbB";
-            this.lbB.Size = new System.Drawing.Size(131, 37);
+            this.lbB.Size = new System.Drawing.Size(90, 26);
             this.lbB.TabIndex = 0;
             this.lbB.Text = "Black: 2";
             // 
@@ -88,38 +90,54 @@
             // 
             this.lbW.AutoSize = true;
             this.lbW.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbW.Location = new System.Drawing.Point(3, 83);
+            this.lbW.Location = new System.Drawing.Point(2, 57);
+            this.lbW.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbW.Name = "lbW";
-            this.lbW.Size = new System.Drawing.Size(135, 37);
+            this.lbW.Size = new System.Drawing.Size(92, 26);
             this.lbW.TabIndex = 1;
             this.lbW.Text = "White: 2";
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 166);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(233, 46);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Turn: Black";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 83);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Counter";
             // 
             // timeDown
             // 
             this.timeDown.AutoSize = true;
             this.timeDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timeDown.Location = new System.Drawing.Point(178, 120);
+            this.timeDown.Location = new System.Drawing.Point(2, 114);
+            this.timeDown.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.timeDown.Name = "timeDown";
-            this.timeDown.Size = new System.Drawing.Size(86, 46);
+            this.timeDown.Size = new System.Drawing.Size(59, 31);
             this.timeDown.TabIndex = 4;
             this.timeDown.Text = "120";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Turn: Black";
+            // 
             // Exitbtn
             // 
+            this.Exitbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.Exitbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Exitbtn.Location = new System.Drawing.Point(3, 215);
+            this.Exitbtn.Location = new System.Drawing.Point(2, 178);
+            this.Exitbtn.Margin = new System.Windows.Forms.Padding(2);
             this.Exitbtn.Name = "Exitbtn";
-            this.Exitbtn.Size = new System.Drawing.Size(137, 49);
+            this.Exitbtn.Size = new System.Drawing.Size(91, 32);
             this.Exitbtn.TabIndex = 5;
             this.Exitbtn.Text = "Exit";
             this.Exitbtn.UseVisualStyleBackColor = true;
@@ -130,24 +148,13 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 120);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(169, 46);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Counter";
-            // 
             // StartFirstScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(999, 758);
+            this.ClientSize = new System.Drawing.Size(666, 493);
             this.Controls.Add(this.flScoreBoard);
             this.Controls.Add(this.PlayBoard);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "StartFirstScreen";
             this.Text = "StartFirst";
             this.Load += new System.EventHandler(this.StartFirstScreen_Load);
